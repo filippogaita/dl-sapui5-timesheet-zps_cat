0337d887432a183f2bbfa679cdd46a6f7d9496b8
filippogaita <88480148+filippogaita@users.noreply.github.com>
2021-08-06 17:07:59 +0200

    Delete old Timesheet Fiori App directory

--- Timesheet Fiori App/dist/test/unit/unitTests.qunit.js
+++
@@ -1,1 +0,0 @@
-QUnit.config.autostart=false;sap.ui.getCore().attachInit(function(){"use strict";sap.ui.require(["zfiori_timesheet_3n/zfiori_timesheet_3n/test/unit/AllTests"],function(){QUnit.start()})});
